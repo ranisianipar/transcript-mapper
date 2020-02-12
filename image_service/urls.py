54882d1cee94fa95b66541759f59a0cbb72e9e1c
Rani Lasma Uli <rani.sianipar@gmail.com>
2020-02-12 09:03:31 +0700

    add some methods

--- image_service/urls.py
+++ image_service/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 from image_service import views
 
 urlpatterns = [
-    url(r'^$', views.translate_image, name='translate_image'),
+    url(r'^$', views.index, name='index'),
 ]
--- image_service/views.py
+++ image_service/views.py
@@ -6,10 +6,36 @@ except ImportError:
 import pytesseract
 import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import base64
+from io import StringIO
 
 
 test = {'requestId':'/storage/2/12apap.png', 'title': 'haha', 'amount': 0}
 
-# Create your views here.
-def translate_image(request):
+request = {'requestId':'/storage/2/12apap.png', 'image': 'haha'}
+
+@csrf_exempt
+def index(request):
+    if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        ocr_result = do_ocr(body['image'])
+        print("OCR RESULT")
+        print(ocr_result)
+        map_result = map_to_transaction(ocr_result)
     return JsonResponse(test)
+
+def map_to_transaction(ocrResult):
+    return None
+
+def do_ocr(image):
+    imgstring = image.split('base64,')[-1].strip()
+    image_string = StringIO(base64.b64decode(imgstring))
+    image = Image.open(image_string)
+    
+    # Overlay on white background, see http://stackoverflow.com/a/7911663/1703216
+    bg = Image.new("RGB", image.size, (255,255,255))
+    bg.paste(image,image)
+
+    return pytesseract.image_to_string(bg)
